627e310aa517f8d822ec96829dbebb2eadc85de6
zarina777 <uktamovazarinabaratovna@gmail.com>
2024-04-25 20:11:38 +0500

    Changed

--- script.js
+++ script.js
@@ -1,5 +1,93 @@
 let app = document.querySelector('.app')
-let h1 = document.createElement('h1')
+let nav = null;
+let calc = null;
+let todo = null;
+let container = null;
 
-h1.textContent= 'Quron new project spa'
-app.append(h1)
+// ---
+
+const history = {
+  push: (url) => {
+    window.history.pushState({}, "", `#/${url}`);
+    sessionStorage.setItem("path", url);
+  },
+  path: () => {
+    return sessionStorage.getItem("path");
+  },
+};
+
+const navObject = {
+  addComponents: () => {
+    nav = document.createElement("nav");
+    calc = document.createElement("button");
+    calc.textContent = "calc";
+    todo = document.createElement("button");
+    todo.textContent = "todo";
+    nav.appendChild(calc);
+    nav.appendChild(todo);
+    app.appendChild(nav);
+  },
+};
+
+const containerObject = {
+  addComponents: () => {
+    container = document.createElement("div");
+    container.setAttribute("class", "container");
+    app.appendChild(container);
+  },
+};
+
+// ----
+
+function Calc() {
+  let h1 = document.createElement("h1");
+  h1.textContent = "Calc";
+
+  container.appendChild(h1);
+}
+
+function Todo() {
+  let h1 = document.createElement("h1");
+  h1.textContent = "To-do List";
+
+  container.appendChild(h1);
+}
+
+function Clear(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+// ----
+(() => {
+  navObject.addComponents();
+  containerObject.addComponents();
+
+  if (history.path()) {
+    if (history.path() === "calc") {
+      window.history.pushState({}, document.title, window.location.pathname);
+      history.push("calc");
+      Calc();
+    } else {
+      todo();
+    }
+  } else {
+    history.push("calc");
+    Calc();
+  }
+})();
+
+// ----
+
+calc.addEventListener("click", () => {
+  history.push("calc");
+  Clear(container);
+  Calc();
+});
+
+todo.addEventListener("click", () => {
+  history.push("todo");
+  Clear(container);
+  Todo();
+});
