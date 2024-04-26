91458cbf3d482284a8db40396c437e50f3ccc84d
zarina777 <uktamovazarinabaratovna@gmail.com>
2024-04-26 08:37:39 +0500

    header is ready

--- script.js
+++ script.js
@@ -1,93 +1,36 @@
 let app = document.querySelector('.app')
-let nav = null;
-let calc = null;
-let todo = null;
-let container = null;
 
-// ---
 
-const history = {
-  push: (url) => {
-    window.history.pushState({}, "", `#/${url}`);
-    sessionStorage.setItem("path", url);
-  },
-  path: () => {
-    return sessionStorage.getItem("path");
-  },
-};
 
-const navObject = {
-  addComponents: () => {
-    nav = document.createElement("nav");
-    calc = document.createElement("button");
-    calc.textContent = "calc";
-    todo = document.createElement("button");
-    todo.textContent = "todo";
-    nav.appendChild(calc);
-    nav.appendChild(todo);
-    app.appendChild(nav);
-  },
-};
+// TOP NAVBAR
+let topNav = document.createElement('div')
+topNav.classList.add('topNav')
+app.append(topNav)
 
-const containerObject = {
-  addComponents: () => {
-    container = document.createElement("div");
-    container.setAttribute("class", "container");
-    app.appendChild(container);
-  },
-};
+// LOGO
+let logo = document.createElement('div')
+logo.classList.add('logo')
+logo.innerHTML=`<img src="./assets/media/logo.png" alt=""> <div class="logoText"><h4>Quran Mazid</h4> <span>Read and Learn the Noble Quran</span></div>`
+topNav.append(logo)
 
-// ----
+// Settings and Mode
+let settings = document.createElement('ul')
+settings.classList.add('settings')
+topNav.append(settings)
 
-function Calc() {
-  let h1 = document.createElement("h1");
-  h1.textContent = "Calc";
+let mode = document.createElement('li')
+mode.innerHTML=`<i class="fa-solid fa-moon"></i>`
+let setting = document.createElement('li')
+setting.innerHTML=`	<i class="fa-solid fa-gear"></i>`
+settings.append(mode,setting)
 
-  container.appendChild(h1);
-}
 
-function Todo() {
-  let h1 = document.createElement("h1");
-  h1.textContent = "To-do List";
+// Side BAR
+let sideBar = document.createElement('div')
+sideBar.classList.add('sidebar')
+app.append(sideBar)
 
-  container.appendChild(h1);
-}
 
-function Clear(element) {
-  while (element.firstChild) {
-    element.removeChild(element.firstChild);
-  }
-}
 
-// ----
-(() => {
-  navObject.addComponents();
-  containerObject.addComponents();
 
-  if (history.path()) {
-    if (history.path() === "calc") {
-      window.history.pushState({}, document.title, window.location.pathname);
-      history.push("calc");
-      Calc();
-    } else {
-      todo();
-    }
-  } else {
-    history.push("calc");
-    Calc();
-  }
-})();
 
-// ----
-
-calc.addEventListener("click", () => {
-  history.push("calc");
-  Clear(container);
-  Calc();
-});
-
-todo.addEventListener("click", () => {
-  history.push("todo");
-  Clear(container);
-  Todo();
-});
