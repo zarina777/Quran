a76e70873c6f97947bcfc25d8c2af213e2f999de
zarina777 <uktamovazarinabaratovna@gmail.com>
2024-04-26 10:54:52 +0500

    v

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ let nav = null
 let home = null
 let mainPage=null
 let saved = null
-
+let container=null
 
 
 
@@ -47,13 +47,13 @@ const history = {
 const sideBar ={
 addComponents:()=>{
   nav = document.createElement('ul');
-  nav.classList.add('sidebar');
+   nav.classList.add('sidebar');
   app.append(nav)
-  home= document.createElement('li')
+   home= document.createElement('li')
   home.innerHTML='<i class="fa-solid fa-house"></i>'
-  mainPage= document.createElement('li')
+   mainPage= document.createElement('li')
   mainPage.innerHTML='<i class="fa-solid fa-book-open"></i>'
-  saved= document.createElement('li')
+   saved= document.createElement('li')
   saved.innerHTML='<i class="fa-solid fa-bookmark"></i>'
   nav.append(home,mainPage,saved)
   for(let i=0;i<nav.children.length;i++){
@@ -61,7 +61,65 @@ addComponents:()=>{
   }
 }
 }
-sideBar.addComponents()
+function getContainer(){
+  container= document.createElement('div')
+  container.setAttribute("class", "container");
+  app.appendChild(container);
+}
+function toHome(){
+  let home = document.createElement('div')
+  home.setAttribute("class", "home");
+  container.append(home)
+}
+function toMainPage(){
+  let mainPage = document.createElement('div')
+  mainPage.setAttribute("class", "mainPage");
+  container.append(mainPage)
+}
+function toSaved(){
+  let saved = document.createElement('div')
+  saved.setAttribute("class", "saved");
+  container.append(saved)
+}
+function Clear(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+(() => {
+  sideBar.addComponents();
+  getContainer()
 
+  if (history.path()) {
+    if (history.path() === "home") {
+      window.history.pushState({}, document.title, window.location.pathname);
+      history.push("home");
+      toHome()
+    } else if(history.path()==="main") {
+      window.history.pushState({}, document.title, window.location.pathname);
+      history.push("main");
+      toMainPage()
+    } else{
+      toSaved()
+    }
+  } else {
+    history.push('main')
+    toMainPage()
+  }
+})();
 
-
+home.addEventListener("click", () => {
+  history.push("home");
+  Clear(container);
+  toHome()
+});
+mainPage.addEventListener("click", () => {
+  history.push("main");
+  Clear(container);
+  toMainPage()
+});
+saved.addEventListener("click", () => {
+  history.push("saved");
+  Clear(container);
+  toSaved()
+});
